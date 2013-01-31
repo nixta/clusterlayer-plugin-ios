1ec6d4060398dffb3ea920ef2349e5f83739a41a
Nicholas Furness <nfurness@nixta.com>
2013-01-31 17:28:31 -0500

    Blank project for ArcGIS Runtime SDK for iOS version 2.x.
    Project is minimally wired up with a blank AGSMapView and ArcGIS Library and dependent libraries referenced.

---
+++ AGSBlankSample/AGSBlankSampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EQSAppDelegate.h
+//  Basemaps
+//
+//  Created by Nicholas Furness on 11/29/12.
+//  Copyright (c) 2012 ESRI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGSBlankSampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AGSBlankSample/AGSBlankSampleViewController.m
@@ -0,0 +1,30 @@
+//
+//  EQSViewController.m
+//  Basemaps
+//
+//  Created by Nicholas Furness on 11/29/12.
+//  Copyright (c) 2012 ESRI. All rights reserved.
+//
+
+#import "AGSBlankSampleViewController.h"
+#import <ArcGIS/ArcGIS.h>
+
+@interface AGSBlankSampleViewController ()
+@property (weak, nonatomic) IBOutlet AGSMapView *mapView;
+@end
+
+@implementation AGSBlankSampleViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
