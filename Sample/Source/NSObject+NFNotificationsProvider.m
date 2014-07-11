d3a1e236fc7cb5fdead74e176fbbba7c7c10a4a7
Nicholas Furness <nfurness@nixta.com>
2014-07-11 14:31:17 -0400

    Update notification registration category

--- Sample/Source/NSObject+NFNotificationsProvider.m
+++ Sample/Source/NSObject+NFNotificationsProvider.m
@@ -1,6 +1,5 @@
 //
 //  NSObject+NFNotificationsProvider.m
-//  ClusterLayerSample
 //
 //  Created by Nicholas Furness on 4/11/14.
 //  Copyright (c) 2014 ESRI. All rights reserved.
@@ -9,23 +8,51 @@
 #import "NSObject+NFNotificationsProvider.h"
 
 @implementation NSObject (NFNotificationsProvider)
-
--(void)registerListener:(id)listener forNotifications:(NSDictionary *)notificationSelectors {
-    for (id notificationName in notificationSelectors) {
-        [[NSNotificationCenter defaultCenter] addObserver:listener
-                                                 selector:NSSelectorFromString(notificationSelectors[notificationName])
-                                                     name:notificationName
-                                                   object:self];
-    }
+#pragma mark - Registration of listeners to self
+-(void)registerListener:(id)listener forNotifications:(NSDictionary *)notificationSelectorsByName {
+    [NSObject __registerListener:listener onObject:self forNotifications:notificationSelectorsByName];
 }
 
 -(void)unRegisterListener:(id)listener fromNotifications:(NSArray *)notificationNames {
-    
-	for (id notificationName in notificationNames) {
-        [[NSNotificationCenter defaultCenter] removeObserver:listener
-                                                        name:notificationName
-                                                      object:self];
+    [NSObject __unRegisterListener:listener onObject:self fromNotifications:notificationNames];
+}
+
+#pragma mark - Registration of self as listener to object or objects
+-(void)registerAsListenerForNotifications:(NSDictionary *)notificationSelectorsByName onObjectOrObjects:(id)objectOrObjects {
+    if (objectOrObjects) {
+        for (id source in [objectOrObjects conformsToProtocol:@protocol(NSFastEnumeration)]?objectOrObjects:@[objectOrObjects]) {
+            [NSObject __registerListener:self onObject:source forNotifications:notificationSelectorsByName];
+        }
+    } else {
+        [NSObject __registerListener:self onObject:nil forNotifications:notificationSelectorsByName];
     }
 }
 
+-(void)unRegisterAsListenerFromNotifications:(NSArray *)notificationNames onObjectOrObjects:(id)objectOrObjects {
+    if (objectOrObjects) {
+        for (id source in [objectOrObjects conformsToProtocol:@protocol(NSFastEnumeration)]?objectOrObjects:@[objectOrObjects]) {
+            [source __unRegisterListener:self onObject:source fromNotifications:notificationNames];
+        }
+    } else {
+        [NSObject __unRegisterListener:self onObject:nil fromNotifications:notificationNames];
+    }
+}
+
+#pragma mark - Internal Methods
++(void)__registerListener:(id)listener onObject:(id)source forNotifications:(NSDictionary *)notificationSelectorsByName {
+    for (id notificationName in notificationSelectorsByName) {
+        [[NSNotificationCenter defaultCenter] addObserver:listener
+                                                 selector:NSSelectorFromString(notificationSelectorsByName[notificationName])
+                                                     name:notificationName
+                                                   object:source];
+    }
+}
+
++(void)__unRegisterListener:(id)listener onObject:(id)source fromNotifications:(NSArray *)notificationNames {
+	for (id notificationName in notificationNames) {
+        [[NSNotificationCenter defaultCenter] removeObserver:listener
+                                                        name:notificationName
+                                                      object:source];
+    }
+}
 @end
